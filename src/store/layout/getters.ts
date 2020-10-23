1d689ea59497d2a0a86e5946a865ff1d4e81b09d
lirongtong <lirongtong@tvflnet.com>
2020-10-23 20:45:43 +0800

    layout 状态管理

--- src/store/layout/getters.ts
+++ src/store/layout/getters.ts
@@ -20,5 +20,25 @@ export const getters: GetterTree<LayoutState, RootState> = {
         let collapsed = state.collapsed
         if (collapsed === null) collapsed = $storage.get(config.caches.storages.collapsed)
         return collapsed
-    }
+    },
+
+    opens: (state): any[] => {
+		return state.opens;
+    },
+    
+    active: (state): string | null => {
+		return state.active;
+	},
+	
+	drawer: (state): boolean => {
+		return state.drawer;
+    },
+    
+    routes: (state): {} => {
+		let routes = state.routes;
+		if (Object.keys(routes).length <= 0) {
+			routes = JSON.parse($storage.get(config.caches.storages.routes) ?? '{}');
+		}
+		return routes;
+	}
 }
---
+++ src/store/layout/mutations.ts
@@ -0,0 +1,73 @@
+import { MutationTree } from 'vuex'
+import { LayoutState, mutations } from '../types'
+import { config } from '../../utils/config'
+import { $storage } from '../../utils/storage'
+
+export const mutation: MutationTree<LayoutState> = {
+    /**
+     * Set the left menu to expand or collapse.
+     * @param state 
+     * @param status 
+     */
+    [mutations.layout.collapsed](state, status: boolean | null = null): void {
+        state.collapsed = status
+        $storage.set(config.caches.storages.collapsed, status)
+    },
+
+    /**
+     * The currently open menu item.
+     * @param state 
+     * @param opens 
+     */
+    [mutations.layout.opens](state, opens: string | any[]) {
+		state.opens = Array.isArray(opens) ? opens: [opens];
+    },
+    
+    /**
+     * The currently selected menu item.
+     * @param state 
+     * @param active 
+     */
+    [mutations.layout.active](state, active: string) {
+		state.active = active;
+    },
+    
+    /**
+     * Whether to open the drawer menu.
+     * @param state 
+     * @param status 
+     */
+    [mutations.layout.drawer](state, status: boolean) {
+		state.drawer = status;
+    },
+
+    /**
+     * Whether is mobile phone.
+     * The visible area on the PC browser is less than 600px, and the value is also `true`.
+     * This value is synchronized with `this.G.mobile`.
+     * @param state 
+     * @param status 
+     */
+    [mutations.layout.mobile](state, status: boolean) {
+		state.mobile = status;
+    },
+    
+    /**
+     * Screen type size (xs, xl, sm ...)
+     * @param state 
+     * @param type 
+     */
+    [mutations.layout.type](state, type: string) {
+		state.type = type;
+	},
+    
+    /**
+     * history routes.
+     * @param state 
+     * @param routes 
+     */
+    [mutations.layout.routes](state, routes: {}) {
+		state.routes = routes;
+		$storage.set(config.caches.storages.routes, JSON.stringify(routes));
+	}
+}
