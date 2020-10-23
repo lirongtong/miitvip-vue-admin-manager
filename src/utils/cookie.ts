8f847e5e78fd6aec8ac3206bc778109a7167be1d
lirongtong <lirongtong@tvflnet.com>
2020-10-23 20:20:25 +0800

    cookie 配置修正，暴露 $cookie，以便其它地方可以单独调用

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -11,26 +11,10 @@
  * +-------------------------------------------+
  */
 import { App } from 'vue'
-import {Router} from 'vue-router'
 
 export declare interface Cookie {
-    /**
-     * get cookie.
-     * @param item 
-     */
     get(item: string): () => any;
-
-    /**
-     * set cookie.
-     * @param item 
-     * @param value 
-     */
     set(item: string, value: any): () => void;
-
-    /**
-     * del cookie.
-     * @param item 
-     */
     del(item: string): () => void;
 }
 
@@ -40,66 +24,74 @@ declare module '@vue/runtime-core' {
     }
 }
 
+class MiCookie {
+
+    prefix = (import.meta as any).env.VITE_MAKEIT_ADMIN_PREFIX ?? 'mi-'
+
+    /**
+     * Get Cookie.
+     * @param key
+     * @return {string|null}
+     */
+    get(key: string): any {
+        const name = `${this.prefix}${key}=`
+        const values = document.cookie.split(';')
+        for (let i = 0, len = values.length; i < len; i++) {
+            let value = values[i]
+            while (value.charAt(0) === ' ') value = value.substring(1)
+            if (value.indexOf(name) !== -1) {
+                return value.substring(name.length, value.length)
+            }
+        }
+        return null
+    }
+
+    /**
+     * Set Cookie.
+     * @param name
+     * @param value
+     * @param expire
+     */
+    set(name: string, value: any, expire: number | null): void {
+        let expires
+        if (expire) {
+            const date = new Date()
+            date.setTime(date.getTime() + (expire * 24 * 60 * 60 * 1000))
+            expires = `expires=${date.toUTCString()}`
+        }
+        const params = [
+            `${this.prefix}${name}=${escape(value)}`,
+            expires,
+            'path=/'
+        ]
+        document.cookie = params.join(';')
+    }
+
+    /**
+     * Delete Cookie.
+     * @param names
+     */
+    del(names: string | any[]): void {
+        if (Array.isArray(names)) {
+            for (let i = 0, len = names.length; i < len; i++) {
+                const name = names[i]
+                if (name) this.set(name, '', -1)
+            }
+        } else {
+            this.set(names, '', -1)
+        }
+    }
+}
+
+export const $cookie = new MiCookie()
+
 const cookie = {
     /**
      * use.
      * @param app 
      */
     install(app: App, prefix: string = 'mi-') {
-        app.config.globalProperties.$cookie = {
-            /**
-             * Get Cookie.
-             * @param key
-             * @return {string|null}
-             */
-            get(key: string): any {
-                const name = `${prefix}${key}=`
-                const values = document.cookie.split(';')
-                for (let i = 0, len = values.length; i < len; i++) {
-                    let value = values[i]
-                    while (value.charAt(0) === ' ') value = value.substring(1)
-                    if (value.indexOf(name) !== -1) {
-                        return value.substring(name.length, value.length)
-                    }
-                }
-                return null
-            },
-            /**
-             * Set Cookie.
-             * @param name
-             * @param value
-             * @param expire
-             */
-            set(name: string, value: any, expire: number | null): void {
-                let expires
-                if (expire) {
-                    const date = new Date()
-                    date.setTime(date.getTime() + (expire * 24 * 60 * 60 * 1000))
-                    expires = `expires=${date.toUTCString()}`
-                }
-                const params = [
-                    `${prefix}${name}=${escape(value)}`,
-                    expires,
-                    'path=/'
-                ]
-                document.cookie = params.join(';')
-            },
-            
-            /**
-             * Delete Cookie.
-             * @param names
-             */
-            del(names: string | any[]): void {
-                if (Array.isArray(names)) {
-                    for (let i = 0, len = names.length; i < len; i++) {
-                        const name = names[i]
-                        if (name) this.set(name, '', -1)
-                    }
-                } else {
-                    this.set(names, '', -1)
-                }
-            }
-        }
+        app.config.globalProperties.$cookie = $cookie
     }
 }
 
