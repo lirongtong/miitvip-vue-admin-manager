d8be9f364b88bc60d00bbd45ee3089efd298b9c7
lirongtong <lirongtong@tvflnet.com>
2020-10-23 21:33:27 +0800

    index 入口中添加 axios 拦截器

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,18 @@
  * +-------------------------------------------+
  */
 import { App } from 'vue'
-import { createStore, useStore } from 'vuex'
+import { createStore } from 'vuex'
+import axios from 'axios'
 import config from './utils/config'
 import cookie from './utils/cookie'
 import storage from './utils/storage'
 import http from './utils/http'
 import tools from './utils/tools'
+
 import { layout } from './store/layout'
 import { passport } from './store/passport'
+import { mutations } from './store/types'
+
 import MiLayout from './components/layout'
 
 const components: {[index: string]: any} = {
@@ -27,7 +31,7 @@ const components: {[index: string]: any} = {
 }
 const env = (import.meta as any).env.NODE_ENV
 let _Vue: boolean | null = null
-let _Ready = null
+let _Ready: boolean | null = null
 const install = (app: App) => {
     Object.keys(components).forEach((name) => {
         app.use(components[name])
@@ -55,8 +59,79 @@ const install = (app: App) => {
             }
         },
 
+        methods: {
+            redirect() {
+                this.$store.commit(`passport/${mutations.passport.reset}`)
+                if (this.$route.name !== 'login') this.$router.push({path: '/login'})
+            }
+        },
+
         mounted() {
-            
+            if (!_Ready) {
+                let regranting = false
+
+                /** request interceptor */
+                axios.interceptors.request.use((config) => {
+                    let token = this.$cookie.get(this.G.caches.cookie.token.access)
+                    if (token) config.headers.Authorization = `Bearer ${token}`
+                    return config
+                }, (err) => {
+                    return Promise.reject(err)
+                })
+
+                /** response interceptor */
+                axios.interceptors.response.use((response) => {
+                    return response
+                }, (err) => {
+                    if (err && err.response) {
+                        const resent = () => {
+                            const config = err.config
+                            const method = config.method.toLowerCase()
+                            return this.$http[method](
+                                config.url,
+                                method === 'get' ? config.params : config.data,
+                                {retryCount: config.retryCount}
+                            )
+                        }
+
+                        /** try again - unauthorized (401) */
+                        if (err.response.status === 401) {
+                            if (!regranting) {
+                                regranting = true
+                                const refresh_token = this.$cookie.get(this.G.caches.cookies.token.refresh)
+                                if (refresh_token) {
+                                    this.$store.dispatch(`passport/refresh`, {refresh_token}).then((res: any) => {
+                                        regranting = false
+                                        if (res.ret.code === 1) return resent()
+                                        else this.redirect()
+                                    }).catch((err: any) => {
+                                        regranting = false
+                                    })
+                                } else {
+                                    regranting = false
+                                    this.redirect()
+                                }
+                            }
+                        } else {
+                            /** retry 3 times, delay 1000ms each time (by default). */
+                            const config = err.config
+                            if (!config || !config.retry) return Promise.reject(err.response)
+                            if (config.retryCount >= config.retry) return Promise.reject(err.response)
+                            config.retryCount += 1
+                            const retry = new Promise(resolve => {
+                                setTimeout(() => {
+                                    resolve()
+                                }, config.retryCount || 1)
+                            })
+                            retry.then(() => {
+                                return resent()
+                            })
+                        }
+                    }
+                })
+
+                _Ready = true
+            }
         }
     })
 }
