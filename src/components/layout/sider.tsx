78d2fd674cdcbc3bd8bf0615d32ed0b44fdf6758
lirongtong <lirongtong@hotmail.com>
2020-11-17 15:49:25 +0800

    左侧 Sider 时间及变量绑定

--- src/components/layout/sider.tsx
+++ src/components/layout/sider.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from 'vue'
 import { Layout } from 'ant-design-vue'
+import { useStore } from 'vuex'
 import PropTypes, { getSlotContent } from '../../utils/props'
+import { mutations } from '../../store/types'
 import MiLayoutSiderLogo from '../logo'
 import MiLayoutSiderMenu from '../menu'
 
@@ -9,6 +11,10 @@ const MiLayoutSider = defineComponent({
     props: {
         logo: PropTypes.any,
         menu: PropTypes.any
+    },
+    setup() {
+        const store = useStore()
+        return { store }
     },
     methods: {
         getPrefixCls() {
@@ -26,12 +32,23 @@ const MiLayoutSider = defineComponent({
                 menu = (<MiLayoutSiderMenu className={`${prefixCls}-menu`}></MiLayoutSiderMenu>)
             }
             return menu
+        },
+        setCollapsed(collapse: boolean) {
+            this.$g.menus.collapsed = collapse
+            this.store.commit(`layout/${mutations.layout.collapsed}`, collapse)
         }
     },
     render() {
         const prefixCls = this.getPrefixCls()
         return (
-            <Layout.Sider class={prefixCls} width="256" breakpoint="lg">
+            <Layout.Sider
+                class={prefixCls}
+                width="256"
+                breakpoint="lg"
+                collapsed={this.$g.menus.collapsed}
+                onCollapse={this.setCollapsed}
+                trigger={null}
+                collapsible={true}>
                 { () => (
                     <>
                         { this.getLogoElem() }
--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -1,12 +1,15 @@
 import { App, Plugin } from 'vue'
 import Menu from './menu'
 import MenuItem from './item'
+import SubMenu from './submenu'
 import MenuItemLink from './link'
 
+Menu.SubMenu = SubMenu
 Menu.Item = MenuItem
 Menu.Link = MenuItemLink
 Menu.install = (app: App) => {
     app.component(Menu.name, Menu)
+    app.component(Menu.SubMenu.name, Menu.SubMenu)
     app.component(Menu.Item.name, Menu.Item)
     app.component(Menu.Link.name, Menu.Link)
     return app
