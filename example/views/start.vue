f5e7157ccd843bd8ceabe7854f58f9f3c6293444
lirongtong <lirongtong@hotmail.com>
2020-12-10 10:48:47 +0800

    Modal 弹窗组件新增主题色边框
    Modal 组件的弹窗效果样式引入方式修正

--- example/views/start.vue
+++ example/views/start.vue
@@ -1,3 +1,21 @@
 <template>
     Getting Started
+    <button @click="handleVisible">test</button>
+    <mi-modal v-model:visible="visible" title="Title" animation="shake">Content</mi-modal>
 </template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    export default defineComponent({
+        data() {
+            return {
+                visible: false
+            }
+        },
+        methods: {
+            handleVisible() {
+                this.visible = !this.visible
+            }
+        }
+    })
+</script>
--- src/components/modal/style/index.ts
+++ src/components/modal/style/index.ts
@@ -1,2 +1,1 @@
-import './anim.less'
 import './index.less'
