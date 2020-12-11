b68021669d17d79ce77a78131e5afddd8fd91dce
lirongtong <lirongtong@hotmail.com>
2020-12-11 10:12:10 +0800

    路由切换后，左侧菜单选中状态未更新的 BUG 修复

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -66,7 +66,7 @@ export default defineComponent({
     watch: {
         $route: function() {
             const active = this.$g.prefix + this.$route.name
-            if (this.$g.menus.active !== [active]) this.$g.menus.active = [active]
+            this.$g.menus.active = [active]
             this.getRelationshipChain()
         }
     },
