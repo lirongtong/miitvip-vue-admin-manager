c1357dfc7e90a77b05598ab393c2b85f792bb1a1
lirongtong <lirongtong@hotmail.com>
2021-01-15 12:13:08 +0800

    Login / Reigster 组件内的 LOGO 大小修正

--- example/views/register.vue
+++ example/views/register.vue
@@ -3,5 +3,5 @@
         :action="api.register"
         :username-verify-action="api.validator.name"
         :email-verify-action="api.validator.email">
-    </mi-register></mi-register>
+    </mi-register>
 </template>
