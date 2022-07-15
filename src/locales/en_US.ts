5f5a15130c5943f1353e02272f5876db0fb93b13
makeit.vip <lirongtong@hotmail.com>
2022-07-15 22:30:37 +0800

    eslint format fixed

--- src/locales/en_US.ts
+++ src/locales/en_US.ts
@@ -1,146 +1,148 @@
 export default {
-    "site": "makeit.vip",
-    "ok": "OK",
-    "cancel": "Cancel",
-    "kind-tips": "Kind Tips",
-    "understood": "Understood",
-    "activate": "Go to activate",
-    "step": {
-        "prev": "Previous",
-        "next": "Next"
+    site: 'makeit.vip',
+    ok: 'OK',
+    cancel: 'Cancel',
+    'kind-tips': 'Kind Tips',
+    understood: 'Understood',
+    activate: 'Go to activate',
+    step: {
+        prev: 'Previous',
+        next: 'Next'
     },
-    "week": {
-        "mon": "Monday",
-        "tues": "Tuesday",
-        "wed": "Wednesday",
-        "thur": "Thursday",
-        "fri": "Friday",
-        "sat": "Saturday",
-        "sun": "Sunday"
+    week: {
+        mon: 'Monday',
+        tues: 'Tuesday',
+        wed: 'Wednesday',
+        thur: 'Thursday',
+        fri: 'Friday',
+        sat: 'Saturday',
+        sun: 'Sunday'
     },
-    "no-data": "No data yet",
-    "theme": {
-        "light": "Light",
-        "dark": "Dark"
+    'no-data': 'No data yet',
+    theme: {
+        light: 'Light',
+        dark: 'Dark'
     },
-    "notice": {
-        "good-day": "Good Day",
-        "no-bugs": "No Bugs",
-        "no-meeting": "No Meeting",
-        "no-business": "No business needs",
-        "fine": "Everything is find"
+    notice: {
+        'good-day': 'Good Day',
+        'no-bugs': 'No Bugs',
+        'no-meeting': 'No Meeting',
+        'no-business': 'No business needs',
+        fine: 'Everything is find'
     },
-    "search": {
-        "searching": "Searching ···",
-        "key": "Please enter keywords",
-        "fail": {
-            "message": "Search could not be completed without source data",
-            "code": "Error code: ",
-            "reason": "Wrong reason: ",
-            "api": "Unknowable error"
+    search: {
+        searching: 'Searching ···',
+        key: 'Please enter keywords',
+        fail: {
+            message: 'Search could not be completed without source data',
+            code: 'Error code: ',
+            reason: 'Wrong reason: ',
+            api: 'Unknowable error'
         }
     },
-    "page": {
-        "prefix": "No.",
-        "prev": "previous page",
-        "next": "next page",
-        "unit": "",
-        "strip": "",
-        "total": "Total:"
+    page: {
+        prefix: 'No.',
+        prev: 'previous page',
+        next: 'next page',
+        unit: '',
+        strip: '',
+        total: 'Total:'
     },
-    "screenfull": {
-        "error": "Full screen failed, please refresh and try again",
-        "capture": "No content to display in full screen",
-        "support": "The current browser does not support full screen operation"
+    screenfull: {
+        error: 'Full screen failed, please refresh and try again',
+        capture: 'No content to display in full screen',
+        support: 'The current browser does not support full screen operation'
     },
-    "captcha": {
-        "loading": "Loading verification code ···",
-        "init": "Initializing verification code ···",
-        "init-error": "Error in init, please try again later",
-        "click": "Click the button to verify",
-        "checking": "Intelligent detection ···",
-        "pass": "Approved",
-        "tip": "Error message",
-        "error": "Error {num} times in a row, please try again later",
-        "move": "Move the slider",
-        "drag": "Drag the circle slider",
-        "dragging": "Drag the slider to flatten the floating image correctly",
-        "close": "Close verification",
-        "refresh": "Refresh verification",
-        "feedback": "Help feedback",
-        "provide": "makeit.vip provide technical support",
-        "success": "It took {take} seconds to complete the image stitching verification",
-        "flatten": "Drag the slider",
-        "verify": "To complete verification"
+    captcha: {
+        loading: 'Loading verification code ···',
+        init: 'Initializing verification code ···',
+        'init-error': 'Error in init, please try again later',
+        click: 'Click the button to verify',
+        checking: 'Intelligent detection ···',
+        pass: 'Approved',
+        tip: 'Error message',
+        error: 'Error {num} times in a row, please try again later',
+        move: 'Move the slider',
+        drag: 'Drag the circle slider',
+        dragging: 'Drag the slider to flatten the floating image correctly',
+        close: 'Close verification',
+        refresh: 'Refresh verification',
+        feedback: 'Help feedback',
+        provide: 'makeit.vip provide technical support',
+        success: 'It took {take} seconds to complete the image stitching verification',
+        flatten: 'Drag the slider',
+        verify: 'To complete verification'
     },
-    "password": {
-        "lv1": "Weak",
-        "lv2": "General",
-        "lv3": "Good",
-        "lv4": "Great",
-        "tip": "Must contain two or more combinations of letters, numbers and special characters.",
-        "placeholder": "Please enter password",
-        "repeat": "Please enter the password again",
-        "strong": "password strength",
-        "size": "{min}-{max} characters, case sensitive, no spaces before or after.",
-        "format": "Letters, numbers, English and special characters.",
-        "different": "Different passwords",
-        "setting": "Please set your password",
-        "least": "At least {min} characters"
+    password: {
+        lv1: 'Weak',
+        lv2: 'General',
+        lv3: 'Good',
+        lv4: 'Great',
+        tip: 'Must contain two or more combinations of letters, numbers and special characters.',
+        placeholder: 'Please enter password',
+        repeat: 'Please enter the password again',
+        strong: 'password strength',
+        size: '{min}-{max} characters, case sensitive, no spaces before or after.',
+        format: 'Letters, numbers, English and special characters.',
+        different: 'Different passwords',
+        setting: 'Please set your password',
+        least: 'At least {min} characters'
     },
-    "anchor": {
-        "text": "Anchor Link",
-        "close": "Close Anchor Link",
-        "stick": {
-            "yes": "Enable suspension",
-            "no": "Cancel suspension"
+    anchor: {
+        text: 'Anchor Link',
+        close: 'Close Anchor Link',
+        stick: {
+            yes: 'Enable suspension',
+            no: 'Cancel suspension'
         }
     },
-    "passport": {
-        "forget": "Forget password",
-        "remember": "Remember Me",
-        "no-account": "No account? ",
-        "has-account": "Already have account? ",
-        "verify": "Please click the button to verify",
-        "username": "Please enter username / email address / mobile number",
-        "email": "Please input the email address",
-        "password": "Please enter the password",
-        "binding": "bind now",
-        "code": "please enter verification code",
-        "sent": "Verification code has been sent",
-        "resend-downtime": "Resend after {sec}s",
-        "resend-tip": "The verification code has been sent to {email}",
-        "resend": "Resend",
-        "update": "Update Password",
-        "success": "Password reset successful",
-        "illegal": "The operation is not allowed",
-        "login": {
-            "title": "Log in",
-            "sign": "Sign up now",
-            "socialite": "Others",
-            "back": "Back to login"
+    passport: {
+        forget: 'Forget password',
+        remember: 'Remember Me',
+        'no-account': 'No account? ',
+        'has-account': 'Already have account? ',
+        verify: 'Please click the button to verify',
+        username: 'Please enter username / email address / mobile number',
+        email: 'Please input the email address',
+        password: 'Please enter the password',
+        binding: 'bind now',
+        code: 'please enter verification code',
+        sent: 'Verification code has been sent',
+        'resend-downtime': 'Resend after {sec}s',
+        'resend-tip': 'The verification code has been sent to {email}',
+        resend: 'Resend',
+        update: 'Update Password',
+        success: 'Password reset successful',
+        illegal: 'The operation is not allowed',
+        login: {
+            title: 'Log in',
+            sign: 'Sign up now',
+            socialite: 'Others',
+            back: 'Back to login'
         },
-        "register": {
-            "title": "Register",
-            "sign": "go to login",
-            "account": "Please set up a user account",
-            "email-valid": "please enter a valid email address",
-            "format": "Only letters + numbers are allowed, 4-16 characters, and start with a letter",
-            "go": "Go to register",
-            "tips": {
-                "special": "<span class='red'>&lt; Note &gt;</span> Login username, once set, it can't be changed.",
-                "structure": "- Consists of <span class='theme'>letters</span>、<span class='theme'>numbers</span> or <span class='theme'>underscores</span>.",
-                "start": "- Only <span class='theme'>start with a letter</span>, for example: makeit.",
-                "length": "- Username length is <span class='theme'>4-16</span> characters."
+        register: {
+            title: 'Register',
+            sign: 'go to login',
+            account: 'Please set up a user account',
+            'email-valid': 'please enter a valid email address',
+            format: 'Only letters + numbers are allowed, 4-16 characters, and start with a letter',
+            go: 'Go to register',
+            tips: {
+                special:
+                    "<span class='red'>&lt; Note &gt;</span> Login username, once set, it can't be changed.",
+                structure:
+                    "- Consists of <span class='theme'>letters</span>、<span class='theme'>numbers</span> or <span class='theme'>underscores</span>.",
+                start: "- Only <span class='theme'>start with a letter</span>, for example: makeit.",
+                length: "- Username length is <span class='theme'>4-16</span> characters."
             }
         }
     },
-    "history": {
-        "close": {
-            "all": "close all",
-            "left": "close left",
-            "right": "close right",
-            "other": "close other"
+    history: {
+        close: {
+            all: 'close all',
+            left: 'close left',
+            right: 'close right',
+            other: 'close other'
         }
     }
 }
--- src/locales/zh_CN.ts
+++ src/locales/zh_CN.ts
@@ -1,146 +1,148 @@
 export default {
-    "site": "麦可易特网",
-    "ok": "确定",
-    "cancel": "取消",
-    "kind-tips": "温馨提示",
-    "understood": "知道了",
-    "activate": "前往激活",
-    "step": {
-        "prev": "上一步",
-        "next": "下一步"
+    site: '麦可易特网',
+    ok: '确定',
+    cancel: '取消',
+    'kind-tips': '温馨提示',
+    understood: '知道了',
+    activate: '前往激活',
+    step: {
+        prev: '上一步',
+        next: '下一步'
     },
-    "week": {
-        "mon": "周一",
-        "tues": "周二",
-        "wed": "周三",
-        "thur": "周四",
-        "fri": "周五",
-        "sat": "周六",
-        "sun": "周日"
+    week: {
+        mon: '周一',
+        tues: '周二',
+        wed: '周三',
+        thur: '周四',
+        fri: '周五',
+        sat: '周六',
+        sun: '周日'
     },
-    "no-data": "暂无符合条件的数据",
-    "theme": {
-        "light": "浅色",
-        "dark": "深色"
+    'no-data': '暂无符合条件的数据',
+    theme: {
+        light: '浅色',
+        dark: '深色'
     },
-    "notice": {
-        "good-day": "美好的一天",
-        "no-bugs": "无缺陷",
-        "no-meeting": "无会议",
-        "no-business": "无需求",
-        "fine": "诸事皆宜"
+    notice: {
+        'good-day': '美好的一天',
+        'no-bugs': '无缺陷',
+        'no-meeting': '无会议',
+        'no-business': '无需求',
+        fine: '诸事皆宜'
     },
-    "search": {
-        "searching": "搜索中 ···",
-        "key": "请输入要搜索的关键词",
-        "fail": {
-            "message": "源数据获取失败，无法完成搜索",
-            "code": "错误代码：",
-            "reason": "错误原因：",
-            "api": "接口发生了不可预知的错误"
+    search: {
+        searching: '搜索中 ···',
+        key: '请输入要搜索的关键词',
+        fail: {
+            message: '源数据获取失败，无法完成搜索',
+            code: '错误代码：',
+            reason: '错误原因：',
+            api: '接口发生了不可预知的错误'
         }
     },
-    "page": {
-        "prefix": "第",
-        "prev": "上一页",
-        "next": "下一页",
-        "unit": "页",
-        "strip": "条",
-        "total": "共"
+    page: {
+        prefix: '第',
+        prev: '上一页',
+        next: '下一页',
+        unit: '页',
+        strip: '条',
+        total: '共'
     },
-    "screenfull": {
-        "error": "全屏失败，请刷新后再试",
-        "capture": "未获取到要全屏展示的内容",
-        "support": "当前浏览器不支持全屏操作"
+    screenfull: {
+        error: '全屏失败，请刷新后再试',
+        capture: '未获取到要全屏展示的内容',
+        support: '当前浏览器不支持全屏操作'
     },
-    "captcha": {
-        "loading": "正在加载验证码 ···",
-        "init": "正在初始化验证码 ···",
-        "init-error": "初始化接口有误，请稍候再试",
-        "click": "点击按钮进行验证",
-        "checking": "智能检测中 ···",
-        "pass": "通过验证",
-        "tip": "错误提示",
-        "error": "已连续错误达 {num} 次，请稍候再试",
-        "move": "请移动滑块，完成验证",
-        "drag": "拖动左边滑块完成上方拼图",
-        "dragging": "拖动滑块将悬浮图像正确拼合",
-        "close": "关闭验证",
-        "refresh": "刷新验证",
-        "feedback": "帮助反馈",
-        "provide": "提供技术支持",
-        "success": "{take}s速度完成图片拼合验证",
-        "flatten": "拖动滑块拼合图片",
-        "verify": "就能验证成功哦"
+    captcha: {
+        loading: '正在加载验证码 ···',
+        init: '正在初始化验证码 ···',
+        'init-error': '初始化接口有误，请稍候再试',
+        click: '点击按钮进行验证',
+        checking: '智能检测中 ···',
+        pass: '通过验证',
+        tip: '错误提示',
+        error: '已连续错误达 {num} 次，请稍候再试',
+        move: '请移动滑块，完成验证',
+        drag: '拖动左边滑块完成上方拼图',
+        dragging: '拖动滑块将悬浮图像正确拼合',
+        close: '关闭验证',
+        refresh: '刷新验证',
+        feedback: '帮助反馈',
+        provide: '提供技术支持',
+        success: '{take}s速度完成图片拼合验证',
+        flatten: '拖动滑块拼合图片',
+        verify: '就能验证成功哦'
     },
-    "password": {
-        "lv1": "弱不禁风",
-        "lv2": "平淡无奇",
-        "lv3": "出神入化",
-        "lv4": "登峰造极",
-        "tip": "需包含字母、数字及特殊字符两种或以上组合",
-        "placeholder": "请输入密码",
-        "repeat": "请再次输入密码",
-        "strong": "密码强度",
-        "size": "{min}-{max}个字符，区分大小写，前后无空格",
-        "format": "字母、数字、英文、下划线等其他特殊字符",
-        "different": "两次密码输入不一致",
-        "setting": "请设置密码",
-        "least": "密码长度至少为{min}个字符"
+    password: {
+        lv1: '弱不禁风',
+        lv2: '平淡无奇',
+        lv3: '出神入化',
+        lv4: '登峰造极',
+        tip: '需包含字母、数字及特殊字符两种或以上组合',
+        placeholder: '请输入密码',
+        repeat: '请再次输入密码',
+        strong: '密码强度',
+        size: '{min}-{max}个字符，区分大小写，前后无空格',
+        format: '字母、数字、英文、下划线等其他特殊字符',
+        different: '两次密码输入不一致',
+        setting: '请设置密码',
+        least: '密码长度至少为{min}个字符'
     },
-    "anchor": {
-        "text": "锚点 Anchor",
-        "close": "关闭锚点链接",
-        "stick": {
-            "yes": "开启固定悬浮",
-            "no": "取消固定悬浮"
+    anchor: {
+        text: '锚点 Anchor',
+        close: '关闭锚点链接',
+        stick: {
+            yes: '开启固定悬浮',
+            no: '取消固定悬浮'
         }
     },
-    "passport": {
-        "forget": "忘记密码",
-        "remember": "保持登录",
-        "no-account": "没有账号？",
-        "has-account": "已有帐号？",
-        "verify": "请点击按钮进行验证码校验",
-        "username": "请输入用户名 / 邮箱地址 / 手机号码",
-        "email": "请输入邮箱地址",
-        "password": "请输入登录密码",
-        "binding": "立即绑定",
-        "code": "请输入验证码",
-        "sent": "验证码已发送",
-        "resend-downtime": "{sec}s后可重新发送",
-        "resend-tip": "验证码已发送至密保邮箱 {email}",
-        "resend": "重新发送",
-        "update": "更新密码",
-        "success": "密码重置成功",
-        "illegal": "当前操作不被允许",
-        "login": {
-            "title": "登录",
-            "sign": "立即注册",
-            "socialite": "快捷登录方式",
-            "back": "返回登录"
+    passport: {
+        forget: '忘记密码',
+        remember: '保持登录',
+        'no-account': '没有账号？',
+        'has-account': '已有帐号？',
+        verify: '请点击按钮进行验证码校验',
+        username: '请输入用户名 / 邮箱地址 / 手机号码',
+        email: '请输入邮箱地址',
+        password: '请输入登录密码',
+        binding: '立即绑定',
+        code: '请输入验证码',
+        sent: '验证码已发送',
+        'resend-downtime': '{sec}s后可重新发送',
+        'resend-tip': '验证码已发送至密保邮箱 {email}',
+        resend: '重新发送',
+        update: '更新密码',
+        success: '密码重置成功',
+        illegal: '当前操作不被允许',
+        login: {
+            title: '登录',
+            sign: '立即注册',
+            socialite: '快捷登录方式',
+            back: '返回登录'
         },
-        "register": {
-            "title": "注册",
-            "sign": "前往登录",
-            "account": "请设置用户账号",
-            "email-valid": "请输入有效的邮箱地址",
-            "format": "仅允许字母+数字，4-16 个字符，且以字母开头",
-            "go": "前往注册",
-            "tips": {
-                "special": "<span class='red'>&lt; 特别提醒 &gt;</span> 登录用户名，一旦设置，则无法更改。",
-                "structure": "- 由 <span class='theme'>字母</span>、<span class='theme'>数字</span> 或 <span class='theme'>下划线</span> 组成。",
-                "start": "- 只能以 <span class='theme'>字母开头</span>，例如：makeit。",
-                "length": "- 用户名长度为 <span class='theme'>4-16</span> 个字符。"
+        register: {
+            title: '注册',
+            sign: '前往登录',
+            account: '请设置用户账号',
+            'email-valid': '请输入有效的邮箱地址',
+            format: '仅允许字母+数字，4-16 个字符，且以字母开头',
+            go: '前往注册',
+            tips: {
+                special:
+                    "<span class='red'>&lt; 特别提醒 &gt;</span> 登录用户名，一旦设置，则无法更改。",
+                structure:
+                    "- 由 <span class='theme'>字母</span>、<span class='theme'>数字</span> 或 <span class='theme'>下划线</span> 组成。",
+                start: "- 只能以 <span class='theme'>字母开头</span>，例如：makeit。",
+                length: "- 用户名长度为 <span class='theme'>4-16</span> 个字符。"
             }
         }
     },
-    "history": {
-        "close": {
-            "all": "关闭全部",
-            "left": "关闭左侧",
-            "right": "关闭右侧",
-            "other": "关闭其它"
+    history: {
+        close: {
+            all: '关闭全部',
+            left: '关闭左侧',
+            right: '关闭右侧',
+            other: '关闭其它'
         }
     }
 }
