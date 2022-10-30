6bfcac23815f8adb29c1278803d0c2241dc14cd5
makeit.vip <lirongtong@hotmail.com>
2022-10-30 22:17:53 +0800

    打包问题修复 - tsconfig.json 加载错误导致

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -17,7 +17,8 @@ const i18n = createI18n({
     locale: DEFAULT_LANG,
     fallbackLocale: DEFAULT_LANG,
     silentTranslationWarn: true,
-    messages: locales
+    messages: locales,
+    globalInjection: true
 }) as any
 
 const setLocale = async (locale?: string, message?: {}) => {
