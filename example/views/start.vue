28cd242a06efc0368a6f06d3d23a3ab1d3aee1e2
makeit.vip <lirongtong@hotmail.com>
2023-06-07 14:41:33 +0800

    卡片样式调整

--- example/views/start.vue
+++ example/views/start.vue
@@ -1,3 +1,21 @@
 <template>
-    Start
+    <a-card class="mi-card mi-card-code" :tab-list="tabs.list">
+        <div v-if="tabs.active === 'effect'">1</div>
+        <div v-if="tabs.active === 'template'">2</div>
+    </a-card>
 </template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const tabs = reactive({
+    list: [{
+        key: 'effect',
+        tab: '实时效果'
+    }, {
+        key: 'template',
+        tab: 'Template'
+    }],
+    active: 'effect'
+})
+</script>
