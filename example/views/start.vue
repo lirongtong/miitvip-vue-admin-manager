28003eedbb190775e582336d6eddaaa16c1fafb2
lirongtong <lirongtong@hotmail.com>
2023-06-07 20:55:37 +0800

    card 样式

--- example/views/start.vue
+++ example/views/start.vue
@@ -1,6 +1,8 @@
 <template>
-    <a-card class="mi-card mi-card-code" :tab-list="tabs.list">
-        <div v-if="tabs.active === 'effect'">1</div>
+    <a-card class="mi-card-tab mi-card-body-custom" :tab-list="tabs.list">
+        <div v-if="tabs.active === 'effect'">
+            <mi-code language="js" :content="js" />
+        </div>
         <div v-if="tabs.active === 'template'">2</div>
     </a-card>
 </template>
@@ -8,13 +10,26 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 
+const js = `import { createApp } from 'vue'
+import MakeitAdminPro from 'makeit-admin-pro'
+import 'makeit-admin-pro/dist/miitvip.min.css'
+
+const app = createApp(App)
+app.use(router)
+app.use(createStore({}))
+app.use(MakeitAdminPro)
+app.mount('#app')`
+
 const tabs = reactive({
     list: [{
         key: 'effect',
         tab: '实时效果'
     }, {
         key: 'template',
-        tab: 'Template'
+        tab: '模版示例'
+    }, {
+        key: 'variables',
+        tab: '封装参数'
     }],
     active: 'effect'
 })
