c3a862680fc0e34743d306f15cf86b3292d955e2
makeit <makeit@makeit.vip>
2024-03-23 08:45:35 +0800

    NProgress 效果调整

--- src/components/theme/Theme.tsx
+++ src/components/theme/Theme.tsx
@@ -1,10 +1,13 @@
-import { defineComponent, inject, watch } from 'vue'
+import { defineComponent, inject, nextTick, watch } from 'vue'
 import { useThemeStore } from '../../stores/theme'
 import { ThemeProps } from './props'
 import { useI18n } from 'vue-i18n'
 import { $g } from '../../utils/global'
 import { $tools } from '../../utils/tools'
 import { $storage } from '../../utils/storage'
+import { useRoute, useRouter } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import MiThemeProvider from './Provider'
 import styled from './style/theme.module.less'
 
@@ -14,10 +17,16 @@ const MiTheme = defineComponent({
     props: ThemeProps(),
     setup(props, { slots }) {
         const { t } = useI18n()
+        const route = useRoute()
+        const router = useRouter()
         const setLocale = inject('setLocale') as any
+
+        // meta
         $tools.setTitle(t('global.meta.title'))
         $tools.setKeywords($g?.keywords || t('global.meta.keywords'), true)
         $tools.setDescription($g.description || t('global.meta.description'), true)
+
+        // theme tokens
         const primaryColor = $storage.get($g.caches.storages.theme.hex)
         const themeType = $storage.get($g.caches.storages.theme.type)
         const moduleThemeVars = $tools.getThemeModuleProperties(styled)
@@ -29,8 +38,33 @@ const MiTheme = defineComponent({
         $tools.createThemeProperties($g.theme.primary)
         const store = useThemeStore()
         store.$patch({ properties: { ...globalThemeVars } })
+
+        // default language
         $g.locale = $tools.getLanguage()
         setLocale($g.locale)
+
+        // nprogress
+        NProgress.configure({
+            easing: 'ease',
+            speed: 1000,
+            showSpinner: false,
+            trickleSpeed: 200
+        })
+
+        // listener router
+        router.beforeEach((_to, _from, next) => {
+            NProgress.start()
+            next()
+        })
+        router.afterEach(() => {
+            if (route?.meta?.title) $tools.setTitle(route?.meta?.title as string)
+            if (route?.meta?.keywords) $tools.setKeywords(route?.meta?.keywords as string)
+            if (route?.meta?.description) $tools.setDescription(route?.meta?.description as string)
+            nextTick().then(() => {
+                NProgress.done(true)
+                NProgress.remove()
+            })
+        })
 
         watch(
             () => [$g?.theme?.type, $g?.theme?.primary],
