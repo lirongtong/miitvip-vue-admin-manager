648542f6c58dbe909b4a95b96a26611248b159ad
lirongtong <lirongtong@hotmail.com>
2024-03-24 11:48:35 +0800

    example pages

--- example/views/pages/login.vue
+++ example/views/pages/login.vue
@@ -1,1 +1,12 @@
-<template>Login Page</template>
+<template>
+    <mi-login :action="() => handleLogin()" />
+</template>
+
+<script setup lang="ts">
+const handleLogin = () => {
+    console.log('login')
+    // do sometion
+    // return `boolean` or `string`
+    return true
+}
+</script>
--- example/views/pages/register.vue
+++ example/views/pages/register.vue
@@ -1,1 +1,12 @@
-<template>Register Page</template>
+<template>
+    <mi-register :action="() => handleRegister()" />
+</template>
+
+<script setup lang="ts">
+const handleRegister = () => {
+    console.log('register')
+    // do sometion
+    // return `boolean` or `string`
+    return true
+}
+</script>
