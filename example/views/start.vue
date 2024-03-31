55752cf9ea742e9362b45032bff97805c353620a
lirongtong <lirongtong@hotmail.com>
2024-03-31 16:01:46 +0800

    发布 Beata.10

--- example/views/start.vue
+++ example/views/start.vue
@@ -1,10 +1,4 @@
 <template>
-    <mi-code language="shell" :content="content" />
-    <mi-code-demo title="基本用法" summary="最基础的用法，只需要配置 action 即可" language="bash" :code="content">
-        <template #result>
-            <mi-login action="v1/login" :footer="false" />
-        </template>
-    </mi-code-demo>
 </template>
 
 <script setup lang="ts">
