71eb638158533dba9abba1be05f84582808aa62f
makeit <makeit@makeit.vip>
2024-06-11 16:19:58 +0800

    captcha 验证码组件 - 校验参数默认无效的问题修正

--- example/views/passport/login.vue
+++ example/views/passport/login.vue
@@ -3,22 +3,7 @@
         action="/v1/login"
         :captcha-setting="{
             initAction: '/v1/captcha/init',
-            verifyMethod: 'post',
-            verifyAction: '/v1/captcha/verify',
-            verifyParams: { key: captchaKey }
+            verifyAction: '/v1/captcha/verify'
         }"
-        @captcha-init="handleCaptchaInit"
         :socialite-setting="{ domain: 'http://local-account.makeit.vip/v1/oauth' }" />
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const captchaKey = ref<string>('')
-
-const handleCaptchaInit = (data?: any) => {
-    if (data?.ret?.code === 200) {
-        if (data?.data?.key) captchaKey.value = data.data.key
-    }
-}
-</script>
--- example/views/passport/register.vue
+++ example/views/passport/register.vue
@@ -3,21 +3,6 @@
         action="/v1/register"
         :captcha-setting="{
             initAction: '/v1/captcha/init',
-            verifyMethod: 'post',
-            verifyAction: '/v1/captcha/verify',
-            verifyParams: { key: captchaKey }
-        }"
-        @captcha-init="handleCaptchaInit" />
+            verifyAction: '/v1/captcha/verify'
+        }" />
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const captchaKey = ref<string>('')
-
-const handleCaptchaInit = (data?: any) => {
-    if (data?.ret?.code === 200) {
-        if (data?.data?.key) captchaKey.value = data.data.key
-    }
-}
-</script>
